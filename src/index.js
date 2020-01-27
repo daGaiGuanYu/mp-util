07b98941fb89089fcbb0099580adb8b7fc309895
weixianfa <372301467@qq.com>
2020-01-27 11:08:29 +0800

    改名字

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 module.exports = {
   getSearchInputPosition: require('./lib/get-search-input-position/index'),
   WxLogin: require('./lib/login/index'),
-  mixin: require('./lib/mixin/index'),
-  request: require('./lib/request/index')
+  Mixin: require('./lib/mixin/index'),
+  Request: require('./lib/request/index')
 }
