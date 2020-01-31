9a8d0fc2e0e2cb2cc7fd8337c214d57261859681
weixianfa <372301467@qq.com>
2020-01-31 17:30:48 +0800

    删除 mixin

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 module.exports = {
   getTitlePosition: require('./lib/get-title-position/index'),
-  Mixin: require('./lib/mixin/index'),
   Request: require('./lib/request/index'),
   Date: require('./lib/date/index')
 }
