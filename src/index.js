c413afa9d197d729cf8d7bf20a59e62b559cb4ab
weixianfa <372301467@qq.com>
2020-01-31 13:36:58 +0800

    Date

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 module.exports = {
   getTitlePosition: require('./lib/get-title-position/index'),
   Mixin: require('./lib/mixin/index'),
-  Request: require('./lib/request/index')
+  Request: require('./lib/request/index'),
+  Date: require('./lib/date/index')
 }
---
+++ src/lib/date/index.js
@@ -0,0 +1,16 @@
+function getTxt(date=new Date(), [dateSplit='-', split='-', timeSplit=':']=[], withSecond){
+  if(date.constructor != Date)
+    throw Error('date 必须是个 Date 对象，任何包裹对象都不好使！')
+  let year = date.getFullYear()
+  let month = date.getMonth() + 1
+  let day = date.getDate()
+  let hour = date.getHours()
+  let minute = date.getMinutes()
+  let second = date.getSeconds()
+
+  return year+dateSplit+month+dateSplit+day+split+hour+timeSplit+minute+(withSecond?(timeSplit+second):'')
+}
+
+module.exports = {
+  getTxt
+}
