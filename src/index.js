25eaf0639347457dd9b8f3735adea27bebec2d56
weixianfa <372301467@qq.com>
2020-02-02 20:24:57 +0800

    update

--- src/index.js
+++ src/index.js
@@ -1,11 +1,8 @@
-const other = require('./lib/other')
-
 module.exports = {
   getTitlePosition: require('./lib/get-title-position/index'),
   Request: require('./lib/request/index'),
   pushGlobalBehavior: require('./lib/global-behavior'),
   getCurrentPage: require('./lib/get-current-page'),
   toast: require('./lib/toast'),
-  handleSequelizeUpsertResult: other.handleSequelizeUpsertResult,
   Date: require('./lib/date/index')
 }
--- src/lib/other.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  handleSequelizeUpsertResult: created => created?'created':'updated'
-}
