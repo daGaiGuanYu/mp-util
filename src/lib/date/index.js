148bb4ec795e5e15f3b04a9491241bc3a10f4d90
weixianfa <372301467@qq.com>
2020-02-03 11:45:37 +0800

    Date.getFromTxt

--- src/lib/date/index.js
+++ src/lib/date/index.js
@@ -1,3 +1,5 @@
+// 向 Moment 低头
+
 function getTxt(date=new Date(), [dateSplit='-', split=' ', timeSplit=':']=[], withSecond){
   if(date.constructor != Date)
     throw Error('date 必须是个 Date 对象，任何包裹对象都不好使！')
@@ -11,6 +13,15 @@ function getTxt(date=new Date(), [dateSplit='-', split=' ', timeSplit=':']=[], w
   return year+dateSplit+month+dateSplit+day+split+hour+timeSplit+minute+(withSecond?(timeSplit+second):'')
 }
 
+function getFromTxt(txt){
+  let [date, time] = txt.split(' ')
+  let [year, month, day] = date.split('-')
+  month = parseInt(month)
+  month--
+  let [hour, min] = time.split(':')
+  return new Date(year, month, day, hour, min)
+}
+
 module.exports = {
-  getTxt
+  getTxt, getFromTxt
 }
