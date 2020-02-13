0df510043184c0e98fb99ccd4aee2920fc1fd96d
weixianfa <372301467@qq.com>
2020-02-13 17:43:33 +0800

    update

--- src/lib/toast.js
+++ src/lib/toast.js
@@ -9,6 +9,22 @@ common.success = function(title='操作成功', cb){
   if(cb){
     if(cb == 'back')
       cb = wx.navigateBack
+    else if(cb.constructor == String){
+      let url = cb
+      cb = function(){
+        wx.navigateTo({ url })
+      }
+    }else{
+      let url = cb.url
+      if(cb.redirect)
+        cb = function(){
+          wx.redirectTo({ url })
+        }
+      else
+        cb = function(){
+          wx.navigateTo({ url })
+        }
+    }
     setTimeout(cb, 1500)
   }
 
